8fb9bc0232c6cac892b409a02ba84dc28965c592
jb <101614021+jbc2313@users.noreply.github.com>
2023-07-25 00:52:06 -0500

    First Commit
    This is the begging..

---
+++ gentoo-based.asm
@@ -0,0 +1,7 @@
+    ; -------------------------------------------------------
+    ; Write "Gentoo is Based" to the console using only system
+    ; To assemble and run:
+    ;
+    ;
+    ;   nasm -felf64 gentoo-based.asm && ld gentoo-based.o && ./a.out
+    ;
