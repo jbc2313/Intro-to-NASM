77b0500aa1b5ee589a6a716d5e4c9f685e5c062d
jb <101614021+jbc2313@users.noreply.github.com>
2023-07-25 00:55:55 -0500

    Update
    Added beginging code

--- gentoo-based.asm
+++ gentoo-based.asm
@@ -1,7 +1,16 @@
-    ; -------------------------------------------------------
+    ; --------------------------------------------------------------------
     ; Write "Gentoo is Based" to the console using only system
     ; To assemble and run:
     ;
     ;
     ;   nasm -felf64 gentoo-based.asm && ld gentoo-based.o && ./a.out
     ;
+    ; ---------------------------------------------------------------------
+
+
+            global      _start
+
+            section     .text
+_start:     mov         rax, 1              ; system call for write
+            mov         rdi, 1              ; file handle 1 is stdout
+            mov         rsi, message        ; address of string to output
