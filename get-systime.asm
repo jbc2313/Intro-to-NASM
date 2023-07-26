e3fea6500878041b567a7a82978733e7d59eb05c
jb <101614021+jbc2313@users.noreply.github.com>
2023-07-26 00:01:57 -0500

    update
    added code to get the size of the message to output and created a file
    for the future

---
+++ get-systime.asm
@@ -0,0 +1,40 @@
+    ; -----------------------------------------------------------
+    ; Lets use the syscall systime to return the time
+    ;
+    ; To assemble and run:
+    ;
+    ;  nasm -felf64 get-systime.asm && ld get-systime.o && ./a.out
+    ;
+    ;
+    ;--------------------------------------------------------------
+
+        section .data
+message:
+        db  "Gentoo is da way", 10          ; message can be changed without updating in program
+
+    
+        global      _start
+
+        section     .text
+_start:
+        mov         rbx, message            ; move address of message string into RBX
+        mov         rax, rbx                ; move address in RBX into RAX as well (both need to point to same memory segment)
+
+nextchar:
+        cmp         byte [rax], 0           ; compare byte pointed at by RAX to 0 (0 is end of string delimiter)
+        jz          finished                ; jump (if the zero flagged has been set) to finished section
+        inc         rax                     ; increment the address in RAX by one byte (if 0 flag isnt set)
+        jmp         nextchar                ; jump to section nextchar
+
+finished:
+        sub         rax, rbx                ; subtract RBX from RAX, result is number of segmets between them in bytes
+                                            ; this works when memory addresses hold the same type
+
+        mov         rdx, rax                ; RAX now equals the nmber of bytes in the string
+        mov         rsi, message            ; rest should be a normal write
+        mov         rax, 1
+        mov         rdi, 1
+        syscall
+        mov         rax, 60
+        xor         rdi, rdi
+        syscall
